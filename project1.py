9cdb7e2686ef355cced82ba9d1ce6532db615163
zoha-fathima <zoha.fathima@icloud.com>
2025-06-27 23:27:55 +0530

    Initial commit

--- project1.py
+++ project1.py
@@ -11,7 +11,7 @@ df = pd.read_csv("diabetes1.csv")
 #Data Visualisation
 counts=df['Outcome'].value_counts()
 counts.plot(kind='bar',color=['green','red'])
-plt.title("Diabedit Prediction")
+plt.title("Diabetic Prediction")
 plt.xticks(ticks=[0,1],labels=['Non-Diabetic','Diabetic'],rotation=0)
 plt.ylabel("No of people")
 plt.show()
@@ -23,11 +23,9 @@ y = df['Outcome']
 # Training the model
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# # Train Logistic Regression model
-# model = LogisticRegression(max_iter=1000)
-# model.fit(X_train, y_train)
 
-# Step 4: Training model
+
+# Training model
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
